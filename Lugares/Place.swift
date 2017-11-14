c68fc9d31676855f0fa76a04cc632011340b564e
Pep Salcedo <salcedo.nerv@gmail.com>
2017-11-13 20:46:08 -0600

    Changes

---
+++ Lugares/Place.swift
@@ -0,0 +1,9 @@
+//
+//  Place.swift
+//  Lugares
+//
+//  Created by Jose Martin Salcedo Lazaro on 11/3/17.
+//  Copyright © 2017 Jose Martin Salcedo Lazaro. All rights reserved.
+//
+
+import Foundation
